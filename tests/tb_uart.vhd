32bdd3eb6615817f35025bb130a417f5a5fe9edd
Leander Schulz <leanderschulz@gmx.de>
2017-11-10 00:11:09 +0100

    tb_uart improved

--- tests/tb_uart.vhd
+++ tests/tb_uart.vhd
@@ -43,38 +43,6 @@ ARCHITECTURE tb_uart_arch OF tb_uart IS
         );
     END COMPONENT e_uart_transmit_mux;
 
---- helper procedures and functions
-    PROCEDURE p_send_byte (
-        SIGNAL s_in  : in  std_logic_vector(7 downto 0);
-        SIGNAL s_rx  : out std_logic
-    ) IS
-    BEGIN
-        s_rx <= '0';            -- start bit
-        WAIT FOR 2000 ns;
-        FOR i IN 0 TO 7 LOOP
-            s_rx <= s_in(i);    -- send bits 0-7
-            WAIT FOR 2000 ns;
-        END LOOP;
-        s_rx <= '1';            -- stop Bit
-        WAIT FOR 2000 ns;
-        s_rx <= '1';            -- idle
-        WAIT FOR 3000 ns;
-    END p_send_byte;
-    
-    --- helper procedures and functions
-    PROCEDURE p_send_point (
-        SIGNAL s_in  : in  std_logic_vector(167 downto 0);
-        SIGNAL s_rx  : out std_logic
-    ) IS
-        SIGNAL s_next : std_logic_vector(7 downto 0);
-    BEGIN
-        FOR i IN 0 TO 21 LOOP
-            s_next <= s_in(i*8 DOWNTO (i+1)*8);
-            p_send_byte(s_next,s_rx)
-        END LOOP;
-    END p_send_point;
-    
-
 --- Internal Signals
     SIGNAL s_clk        : std_logic;
     SIGNAL s_rst        : std_logic := '0'; 
@@ -89,9 +57,9 @@ ARCHITECTURE tb_uart_arch OF tb_uart IS
     SIGNAL s_verify     : std_logic := '0';
     
 --- test cases
-    CONSTANT c_r_1 : std_logic_vector(167 DOWNTO 0) := x"0796d7796309aee88283dbabf698ba6b6f8e26de11ae";
-    CONSTANT c_s_1 : std_logic_vector(167 DOWNTO 0) := x"070871e308d5acbc5064bd42e14a021862ebbc85eea6";
-    CONSTANT c_m_1 : std_logic_vector(167 DOWNTO 0) := x"074848656c6c6f20454344534120576f726c64212121";
+    SIGNAL s_r_1 : std_logic_vector(167 DOWNTO 0) := x"07d7796309aee88283dbabf698ba6b6f8e26de11ae";
+    SIGNAL s_s_1 : std_logic_vector(167 DOWNTO 0) := x"0771e308d5acbc5064bd42e14a021862ebbc85eea6";
+    SIGNAL s_m_1 : std_logic_vector(167 DOWNTO 0) := x"0748656c6c6f20454344534120576f726c64212121";
 
 BEGIN
 --- Instance of Receiver
@@ -128,8 +96,70 @@ BEGIN
         WAIT FOR 10 ns;
     END PROCESS p_clk;
     
---- testing process
-    rx_gen : PROCESS
+    rx_gen : PROCESS IS
+        --- helper procedure
+        PROCEDURE p_send (
+            SIGNAL s_mode_i : IN  std_logic;
+            SIGNAL s_r      : IN  std_logic_vector(167 DOWNTO 0);
+            SIGNAL s_s      : IN  std_logic_vector(167 DOWNTO 0);
+            SIGNAL s_m      : IN  std_logic_vector(167 DOWNTO 0);
+            SIGNAL s_rx     : OUT std_logic
+        ) IS
+        BEGIN
+            -- send mode
+            ASSERT FALSE REPORT "Send Mode" SEVERITY NOTE;
+            s_rx <= '0';        -- Start Bit
+            WAIT FOR 2000 ns;
+            FOR i IN 0 TO 7 LOOP
+                s_rx <= s_mode_i;
+                WAIT FOR 2000 ns;
+            END LOOP;
+            s_rx <= '1';        -- stop bit and idle
+            WAIT FOR 5000 ns;
+            IF s_mode_i = '1' THEN
+                -- send r
+                ASSERT FALSE REPORT "Send Point R" SEVERITY NOTE;
+                FOR i IN 20 DOWNTO 0 LOOP
+                    s_rx <= '0';        -- Start Bit
+                    WAIT FOR 2000 ns;
+                    FOR j IN 0 TO 7 LOOP
+                        s_rx <= s_r(i*8+j); -- send bits 0-7
+                        WAIT FOR 2000 ns;
+                    END LOOP;
+                    s_rx <= '1';        -- stop bit and idle
+                    WAIT FOR 5000 ns;            
+                
+                END LOOP;
+                -- send s
+                ASSERT FALSE REPORT "Send Point S" SEVERITY NOTE;
+                FOR i IN 20 DOWNTO 0 LOOP
+                    s_rx <= '0';        -- Start Bit
+                    WAIT FOR 2000 ns;
+                    FOR j IN 0 TO 7 LOOP
+                        s_rx <= s_s(i*8+j); -- send bits 0-7
+                        WAIT FOR 2000 ns;
+                    END LOOP;
+                    s_rx <= '1';        -- stop bit and idle
+                    WAIT FOR 5000 ns;            
+                
+                END LOOP;
+            END IF;
+            -- send m
+            ASSERT FALSE REPORT "Send Message" SEVERITY NOTE;
+            FOR i IN 20 DOWNTO 0 LOOP
+                s_rx <= '0';        -- Start Bit
+                WAIT FOR 2000 ns;
+                FOR j IN 0 TO 7 LOOP
+                    ASSERT (i*8+j)<168 SEVERITY WARNING;
+                    s_rx <= s_m(i*8+j); -- send bits 0-7
+                    WAIT FOR 2000 ns;
+                END LOOP;
+                s_rx <= '1';        -- stop bit and idle
+                WAIT FOR 5000 ns;            
+            END LOOP;
+        END p_send;
+    
+    -- BEGIN TESTING ---
     BEGIN 
         -- intitialize
         s_rx <= '1';
@@ -139,11 +169,15 @@ BEGIN
 		s_rst <= '0';
         WAIT FOR 880 ns;
         
-        --begin testing
-        p_send_point(c_r_1);
-        p_send_point(c_s_1);
-        p_send_point(c_m_1);
+        -- Test Case 1
+        s_mode <= '0';
+        p_send(s_mode,s_r_1,s_s_1,s_m_1,s_rx); -- ignoring r and s
         
+        WAIT FOR 100 us;
+        
+        -- Test Case 2
+        s_mode <= '1';
+        p_send(s_mode,s_r_1,s_s_1,s_m_1,s_rx);
         
 
         WAIT;
