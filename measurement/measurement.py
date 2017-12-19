5d44455224273891fc6fe3b363ce522f1a131646
Leander Schulz <leanderschulz@gmx.de>
2017-12-19 10:27:14 +0100

    Python Measurements modified

--- measurement/measurement.py
+++ measurement/measurement.py
@@ -1,10 +1,39 @@
 import time
 import serial
 import csv
+import os
+import string
+import binascii
 
 # Number of measurements
 MEASUREMENTS = 1
+# ECDSA Key Length
+M = 163
+# Output file path
 CSV_FILE = "vhdl_measurements.csv"
+# Bytes (B) and Byte-Alignment (A) of Key
+if (M % 8) > 0:
+    Aligned = False
+    Bytes = (M/8)+1
+else:
+    Aligned = True
+    Bytes = (M/8)
+
+def generate_message(M=M):
+    '''
+        Generates a B byte long string
+        with leading zeros if M is not byte aligned
+    '''
+    rand = binascii.b2a_hex(os.urandom(Bytes))
+    rand = bin(int(rand,16))[2:].zfill(Bytes*8)
+    if not Aligned:
+        # fill leading bits with zero
+        tmp = rand[:M]
+        rand = tmp.zfill(Bytes*8)
+    rand = '%08X' % int(rand, 2)
+    while len(rand) < (2*Bytes):
+        rand = "0" + rand
+    return rand
 
 def read_uart(ser):
     """
@@ -15,12 +44,12 @@ def read_uart(ser):
 
     Return readed string
     """
-        out = ''
-        # Wait until all chars are readed
-        while ser.inWaiting() > 0:
-            out += ser.read(1)
-        
-        return out
+    out = ''
+    # Wait until all chars are readed
+    while ser.inWaiting() > 0:
+        out += ser.read(1)
+    print "OUT:" + str(out.encode('hex'))
+    return out
 
 def measure_sign(ser, message):
     """
@@ -29,15 +58,18 @@ def measure_sign(ser, message):
     Args:
         ser (serial.Serial):  handle to uart
         message (string): message to sign
-    
+
     Return signature and elapsed time
     """
-    mode = 0
+    mode = '\0'
     start_time = time.time()
 
     # Send data to device
+    #ser.write(mode)# + '\r\n')
     ser.write(mode + '\r\n')
+    #ser.write(message)# + '\r\n')
     ser.write(message + '\r\n')
+    #ser.flush()
 
     # Reading signature from uart
     r = read_uart(ser)
@@ -45,7 +77,7 @@ def measure_sign(ser, message):
 
     end_time = time.time()
 
-    return (r, s), end_time-start_time
+    return (r.encode('hex'), s.encode('hex')), (end_time-start_time)
 
 def measure_verify(ser, message, (r, s)):
     """
@@ -55,52 +87,51 @@ def measure_verify(ser, message, (r, s)):
         ser (serial.Serial):  handle to uart
         message (string): message to verify
         signature (string): signature of message
-    
+
     Return verify result and elapsed time
     """
-    mode = 1
+    mode = "FF".decode('hex')
     start_time = time.time()
 
     ser.write(mode + '\r\n')
-    ser.write(message + '\r\n')
     ser.write(r + '\r\n')
     ser.write(s + '\r\n')
-    
+    ser.write(message + '\r\n')
+
     # Reading signature from uart
     res = read_uart(ser)
-    
-    end_time = time.time()
-    
-    return res, end_time-start_time
-    
-# Configure the serial connections
-ser = serial.Serial(
-    port='/dev/ttyUSB1',
-    baudrate=9600,
-    parity=serial.PARITY_ODD,
-    stopbits=serial.STOPBITS_TWO,
-    bytesize=serial.SEVENBITS
-)
 
-# Try to open UART connection
-ser.open()
-ser.isOpen():
+    end_time = time.time()
+    print res
+
+    return res, end_time-start_time
+
+# configure serial connection
+ser = serial.Serial(port='COM1',baudrate=9600,bytesize=8,parity='N',stopbits=1)
 
 for i in range(0, MEASUREMENTS):
-    # -- SIGN -----------------------------------
-
     # Generate random hash as message and sign it by device
-    message = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
+    message = generate_message()
+    print message
+
     (r,s), sign_time = measure_sign(ser, message)
 
-    # Let's wait some time before verify
+    print len(r)
+
+    # wait before verify
     time.sleep(1)
 
     # Verify signature
     result, verify_time = measure_verify(ser, message, (r,s))
 
-    # Store results
-    with open(CSV_FILE, 'wb') as csvfile:
-        writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        #writer.writerow(["ID", "R", "S", "VTIME", "V", "VTIME"])
-        writer.writerow([i, r, s, sign_time, result, verify_time])    
+    print "r:" + str(r)
+    print "s:" + str(s)
+    print "sign_time:" + str(sign_time)
+    print "result:" + str(result.encode('hex'))
+    print "verify_time:" + str(verify_time)
+
+    # save to csvfile
+#    with open(CSV_FILE, 'wb') as csvfile:
+#        writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+#        #writer.writerow(["ID", "R", "S", "VTIME", "V", "VTIME"])
+#        writer.writerow([i, r, s, sign_time, result, verify_time])
